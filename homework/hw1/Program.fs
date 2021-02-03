dbac0e24a43cbd56fc6ec1618532f23ac7870c19
Julian Saldana <julianesaldana@yahoo.com>
2021-02-02 18:26:37 -0800

    fixed hw1
    optimized functions to reach 2 million faster

--- homework/hw1/Program.fs
+++ homework/hw1/Program.fs
@@ -38,44 +38,48 @@ let isHit target distance =
         -1
 
 // program 2
-let isPrime num = 
+let isPrime (num : float) = 
     let mutable result = true
-    let mutable i = 2
+    let mutable i = 2.0
 
-    while i < num && result = true do
-        if num % i = 0 then
+    while i * i <= num && result = true do
+        if num % i = 0.0 then
             result <- false
         else
-            i <- i + 1
+            i <- i + 1.0
     result
 
-let sumPrimes max =
-    let mutable sum = 0
-    let mutable i = 3
+let sumPrimes (max : float) =
+    printfn "Starting...."
+    let mutable sum = 0.0
+    let mutable i = 3.0
+    printfn "Processing"
     while i < max do
         if isPrime i then
             sum <- sum + i
-        i <- i + 1
-    sum <- sum + 2
+        i <- i + 1.0
+    sum <- sum + 2.0
     sum
 
 [<EntryPoint>]
 let main argv =
-    // program 1
-    printfn "Program 1=\n"
-    let placeTarget = placeTarget()
-    let mutable result = 1
+    //// program 1
+    //printfn "Program 1=\n"
+    //let placeTarget = placeTarget()
+    //let mutable result = 1
 
-    while result <> 0 do
-        printfn "Target is placed at: %f\n" placeTarget
-        let angle = getAngle()
-        let gunpowder = getGunpowder()
-        let distance = calculateDistance angle gunpowder
-        result <- isHit placeTarget distance
+    //while result <> 0 do
+    //    printfn "Target is placed at: %f\n" placeTarget
+    //    let angle = getAngle()
+    //    let gunpowder = getGunpowder()
+    //    let distance = calculateDistance angle gunpowder
+    //    result <- isHit placeTarget distance
 
     // program 2
     printfn "Program 2=\n"
-    printfn "Is 5 a prime number? %b" (isPrime 5)
-    printfn "Sum of prime numbers up to 2 million: %d" (sumPrimes 2000000)
+    printfn "Choose a number:"
+    let userNum = Console.ReadLine() |> float
+    printfn "\nIs %.2f a prime number? %b" userNum (isPrime 5.0)
+    printfn "Sum of prime numbers up to %.2f: %.2f" userNum (sumPrimes userNum)
 
     0 // return an integer exit code
