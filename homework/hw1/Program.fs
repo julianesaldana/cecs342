e0b9d368a391a4ade54d5d5a39a7846430c6aa98
Julian Saldana <julianesaldana@yahoo.com>
2021-02-03 01:00:10 -0800

    FINAL HW1
    Fixed equations, added header and comments, fully flushed out

--- homework/hw1/Program.fs
+++ homework/hw1/Program.fs
@@ -1,14 +1,17 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿// Julian Saldana 018462169
+// CECS 342 SEC 11
+// HW-1, DUE FEB 3 @ 10AM
 
 open System
 
 // program 1
+
 let placeTarget () =
     let target = Random().NextDouble() * (float 1000)
     target
     
 let getAngle () =
-    let mutable angle = 100 |> float
+    let mutable angle = 100 |> float    // forced angle to be float type
     while angle >= 90.0 || angle <= 0.0 do
         printfn "Enter an angle between 0 and 90 degrees: "
         angle <- Console.ReadLine() |> float
@@ -19,17 +22,17 @@ let getGunpowder () =
     let gunpowder = Console.ReadLine() |> float
     gunpowder
 
-let calculateDistance angle gunpowder =
+let calculateDistance angle gunpowder = // takes angle and gunpowder as parameters
     printfn ("\nCalculating distance...")
     let rads = (angle * Math.PI) / 180.0
-    let distance = (Math.Pow((gunpowder * 30.0), 2.0) * Math.Sin(2.0 * rads)) / (2.0 * 9.81)
+    let distance = (Math.Pow((gunpowder * 30.0), 2.0) * Math.Sin(2.0 * rads)) / (9.81)  // formula needed to calculate distance
     printfn "Distance: %f" distance
     distance
 
 let isHit target distance = 
-    if target - distance <= 1.0 && target - distance >= -1.0 then
+    if target - distance <= 1.0 && target - distance >= -1.0 then   // checking if the target and distance are within 1 meter, this takes into account both possible ways that they can be within close range
         printfn "\nTarget hit!\n------------------------------------------------------------------------------\n"
-        0
+        0   // decided to return int values because i can use these in main to continue looping
     elif distance > target then
         printfn "\nNot hit, long\n------------------------------------------------------------------------------"
         1
@@ -37,27 +40,29 @@ let isHit target distance =
         printfn "\nNot hit, short\n------------------------------------------------------------------------------"
         -1
 
+
 // program 2
-let isPrime (num : float) = 
+
+let isPrime (num : float) = // forcing float because int would not be able to accurately contain increasingly larger numbers
     let mutable result = true
     let mutable i = 2.0
 
-    // while loop follows condition from instructions about squared values
+    // while loop follows condition from instructions about squared values, also makes the program run much faster as tested
     while i * i <= num && result = true do
-        if num % i = 0.0 then
+        if num % i = 0.0 then   // if there is no remainder, not prime because it has a factor other than 1
             result <- false
         else
-            i <- i + 1.0
-    result
+            i <- i + 1.0    // counter to keep going
+    result  // returns boolean value
 
 let sumPrimes (max : float) =
     let mutable sum = 0.0
-    let mutable i = 3.0
+    let mutable i = 3.0 // specified starting point in the instructions
     while i < max do
-        if isPrime i then
+        if isPrime i then   // checks if prime, then adds to sum
             sum <- sum + i
         i <- i + 1.0
-    sum <- sum + 2.0
+    sum <- sum + 2.0    // adds 2 (a prime number) to the very end since the loop doesnt account for it
     sum
 
 [<EntryPoint>]
@@ -67,18 +72,18 @@ let main argv =
     let placeTarget = placeTarget()
     let mutable result = 1
 
-    while result <> 0 do
+    while result <> 0 do    // checks return int from function, if result = 0 (target hit) then it will stop looping
         printfn "Target is placed at: %f\n" placeTarget
         let angle = getAngle()
         let gunpowder = getGunpowder()
         let distance = calculateDistance angle gunpowder
-        result <- isHit placeTarget distance
+        result <- isHit placeTarget distance    // return code will be updated
 
     // program 2
     printfn "Program 2=\n"
     printfn "Choose a number:"
     let userNum = Console.ReadLine() |> float
-    printfn "\nIs %.2f a prime number? %b" userNum (isPrime userNum)
+    printfn "\nIs %.2f a prime number? %b" userNum (isPrime userNum)    // formatted floats used because userNum will be a float and have lots of trailing zeroes
     printfn "Sum of prime numbers up to %.2f: %.2f" userNum (sumPrimes userNum)
 
     0 // return an integer exit code
