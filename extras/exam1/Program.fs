32780ad3fb8d155382e6fa146cdcdc2e86a9405b
Julian Saldana <julianesaldana@yahoo.com>
2021-02-22 12:45:15 -0800

    fasdfsd

---
+++ extras/exam1/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+type OptionalInt = 
+    | None
+    | Integer of int
+
+[<EntryPoint>]
+let main argv =
+    let hasValue opt =
+        match opt with
+        | None -> false
+        | _ -> true
+    0 // return an integer exit code
