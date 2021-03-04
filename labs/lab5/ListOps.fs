ed46aa30d4ad2b925f2877abaee520a6b39d1333
Julian Saldana <julianesaldana@yahoo.com>
2021-03-03 23:08:45 -0800

    added lab6

--- labs/lab5/ListOps.fs
+++ labs/lab5/ListOps.fs
@@ -3,27 +3,11 @@
 
 module ListOps
 
-let reversetail coll =
-    let rec reverseimpl coll acc = // acc = the reverse of the elements that came before this list.
-        
-        match coll with 
-        | [] -> acc // if we've reached the end of the list, return the reverse of the elements that came before this list (all of them).
-
-        // otherwise, the reverse of the list h :: t is....
-        //      h followed by the reverse of the elements that came before it.
-        | h :: t -> reverseimpl t (h :: acc) 
-        
-
-    reverseimpl coll []
-
 let suffixes coll =
     let rec suffixes' coll acc =
-        //let temp = (reversetail acc)
         match coll with
         | [] -> [] :: acc
         | head ::  tail ->
             suffixes' tail (coll :: acc)
 
     suffixes' coll [] |> List.rev
-    
-
---
+++ labs/lab6/ListOps.fs
@@ -0,0 +1,40 @@
+﻿// Ashur Motlagh 018319910
+// Julian Saldana 018462169
+
+module ListOps
+
+type Account =
+    | Balance of int
+    | Overdrawn of int
+    | Empty
+
+type Customer = { Name: string; Account: Account }
+
+let makeCustomerWithBalance name (amount: int) =
+    let account =
+        if amount > 0 then
+            Balance amount
+        elif amount < 0 then
+            Overdrawn(abs amount)
+        else
+            Empty
+    { Name = name; Account = account }
+
+let unknownCustomer = makeCustomerWithBalance "Unknown" 0
+
+
+let totalOverdrawn (name: string) (coll: Customer list) =
+    let customers = coll |> List.filter(fun customer -> 
+    match customer.Account with
+        | Overdrawn o -> true
+        | _ -> false ) |>List.filter(fun customer-> customer.Name = name) |> List.map(fun customer -> customer.Account) |> List.sumBy(fun (Overdrawn x) -> x)
+    customers
+
+let maxBalance (name: string) (coll: Customer list) = 
+    let customers = coll |> List.filter(fun customer -> 
+    match customer.Account with
+        | Balance o -> true
+        | _ -> false ) |>List.filter(fun customer-> customer.Name = name)
+    match customers with
+    | [] -> unknownCustomer
+    | _ ->  customers |> List.max
