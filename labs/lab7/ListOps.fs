63d2b6aad39c01295ba80392b01df3ef266e222d
Julian Saldana <julianesaldana@yahoo.com>
2021-03-15 11:49:28 -0700

    added correct functions

--- labs/lab7/ListOps.fs
+++ labs/lab7/ListOps.fs
@@ -24,9 +24,40 @@ let deposit amount acc =
     | Overdrawn _ -> Empty
 
 let join separator listOfStrings =
-    let result = listOfStrings |> List.fold (+) separator
-    result
+    match coll with
+    | [] -> ""
+    | head :: tail -> List.fold (fun before after -> before + delim + after) head tail
+
+let simplifyBank customers names =
+    // assumption is that customers represents a single customer
+    let reduceCustomer customers = 
+        let combineCustomer left right =
+            match right.Account with
+            | Balance b -> {left with Account = deposit b left.Account}
+            | Overdrawn o -> {left with Account = withdraw b left.Account}
+            | Empty -> left // will choose original if dupe is empty/0
+        
+        List.reduce combineCustomer customers
+
+    let simplifyCustomer acc name =
+        (List.filter (fun c -> c.Name = name) customers
+            |> reduceCustomer)
+            :: acc
+
+    List.fold simplifyCustomer [] names |> List.rev
+
+
+// my join method WITH ERROR
+// let join (separator: string) (listOfStrings: string list) =
+//     listOfStrings
+//     |> List.fold (fun before after -> before + separator + after) ""
     
-let simplifyBank customers names =      // taken from maxBalance function from lab6
-    match customers with
-    | head :: tail 
+// my simplifyBank method
+// let simplifyBank (bank: Customer list) (names: string list) = 
+//     let bankBalance = 
+//         names |> List.map(fun x -> bank |> List.filter (fun y -> x = y.Name))
+//               |> List.map(fun x -> List.fold (fun accum y -> match y.Account with
+//                                                                 |Overdrawn o -> {Name = x.[0].Name ; Account = withdraw o accum.Account}
+//                                                                 |Balance b -> {Name = x.[0].Name ; Account = deposit b accum.Account}
+//                                                                 | _ -> {Name = x.[0].Name ; Account =deposit 0 accum.Account}) {Name = x.[0].Name ;
+//     bankBalance
