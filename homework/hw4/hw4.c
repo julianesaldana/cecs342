3203b060706a4cdfacc7264403345432ca534fcb
Julian Saldana <julianesaldana@yahoo.com>
2021-04-15 19:11:58 -0700

    started on hw4

--- homework/hw4/hw4.c
+++ homework/hw4/hw4.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-const int overhead_size = sizeof(struct Block);
-const int pointer_size = sizeof(void*);
-struct Block *free_head;
-
 struct Block {
     int block_size; // # of bytes in the data section
     struct Block *next_block;   // in C, you have to use "stuct Block" as the type;
 };
 
-void my_initialize_heap(int size) {
+// void my_initialize_heap(int size) {
     
-}
+// }
+
+const int overhead_size = sizeof(struct Block);
+const int pointer_size = sizeof(void*);
+struct Block *free_head;
+
 
 int main() {
     printf("overhead_size = %d\n", overhead_size);
