20d45f03544738d2ab961dadf94dac2891f15825
Julian Saldana <julianesaldana@yahoo.com>
2021-04-17 00:30:29 -0700

    small

---
+++ homework/hw4/cmake-build-debug/CMakeFiles/hw4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw4.dir/main.c.obj"
+  "hw4.exe"
+  "hw4.exe.manifest"
+  "hw4.pdb"
+  "libhw4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hw4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- homework/hw4/main.c
+++ homework/hw4/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Block {
+    // vvv known as the HEADER of the block vvv
     int block_size; // # of bytes in the data section
     struct Block *next_block;   // in C, you have to use "stuct Block" as the type;
 };
@@ -11,10 +13,16 @@ struct Block *free_head;
 
 void my_initialize_heap(int size) {
     free_head = malloc(size);
-    free_head->block_size = 
+    free_head->block_size = size;
+    struct Block *next_head = NULL;
+    free_head->next_block = next_head;
 }
 
 void* my_alloc(int size) {
+
+}
+
+void my_free(void *data) {
 
 }
 
